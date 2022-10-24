4213df2efe316ad8031e0d70dc23d216f3d41441
Moodybleu <meganbenn27@icloud.com>
2022-10-24 08:30:58 -0700

    Adds skills to my about page

--- pages/about.js
+++ pages/about.js
@@ -6,17 +6,29 @@ export default function About() {
         <div>
             <h1>About me</h1>
             <p className={styles.blueBaby} >Hello, My name is Megan. Welcome to my portfolio</p>
-            <h2>Skills:</h2>
-            <ul>
-                <li></li>
-            </ul>
-
             <Image className={styles.image}
                 src='/retouched_Megan.png'
                 alt='Megan'
                 width={350}
                 height={300}
              />   
+            <h2>Skills:</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>Node.js</li>
+                <li>SQL</li>
+                <li>Git</li>
+                <li>Github</li>
+                <li>Postman</li>
+                <li>React Native</li>
+                <li>MERN stack</li>
+                <li>Python</li>
+                <li>MongoDB</li>
+            </ul>
+
+            
              
         </div>
     )
